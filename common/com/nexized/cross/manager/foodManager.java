0de597c34997e0647557486d85dd913feb3bf0da
ModAttackz <chirpzay1@gmail.com>
2013-08-14 10:15:18 -0700

    Update #1 on Food
    Right now I am just making the outline of the food and I will get to the hunger bars filled soon. Do I keep "Name" after the foods under public static void. Oh and how do I add textures?

--- common/com/nexized/cross/manager/foodManager.java
+++ common/com/nexized/cross/manager/foodManager.java
@@ -29,7 +29,8 @@ public class foodManager {
 	public static Item foodLettuce;
 	public static Item foodEggplant;
 	public static Item foodEggScramled;
-	
+	public static Item foodRawbacon;
+	public static Item foodCookedbacon;
     public static void doWork(confEntity idm, confLocalisation local) {
     	// @Add Food Items
     	addFood(idm);
@@ -40,9 +41,27 @@ public class foodManager {
     public static void addFood(confEntity idm) {
     	foodRawbacon = (new crossFood(idm.foodRawbaconID, 1, 0.1F, true).setUnlocalizedName("foodRawbacon"));
     	foodBacon = (new crossFood(idm.foodBaconID, 3, 0.3F, true).setUnlocalizedName("foodBacon"));
+    	foodCorn = (new crossFood(idm.foodCornID, 1, 0.1F, true).setUnlocalizedName("foodCorn"));
+    	foodTomato = (new crossFood(idm.foodTomatoID, 3, 0.3F, true).setUnlocalizedName("foodTomato"));
+    	foodStrawberry = (new crossFood(idm.foodStrawberryID, 1, 0.1F, true).setUnlocalizedName("foodStrawberry"));
+    	foodBlueberry = (new crossFood(idm.foodBlueberryID, 3, 0.3F, true).setUnlocalizedName("foodBlueberry"));
+    	foodGreenTomato = (new crossFood(idm.foodGreenTomatoID, 1, 0.1F, true).setUnlocalizedName("foodGreenTomato"));
+    	foodOrange = (new crossFood(idm.foodOrangeID, 3, 0.3F, true).setUnlocalizedName("foodOrange"));
+    	foodLettuce = (new crossFood(idm.foodLettuceID, 1, 0.1F, true).setUnlocalizedName("foodLettuce"));
+    	foodEggplant = (new crossFood(idm.foodEggplantID, 3, 0.3F, true).setUnlocalizedName("foodEggplant"));
+    	foodEggScramled = (new crossFood(idm.foodEggScramledID, 3, 0.3F, true).setUnlocalizedName("foodEggScramled"));
     }
     
     public static void addNames(confLocalisation local) {
+    	LanguageRegistry.addName(foodCorn, local.foodCornName);
+    	LanguageRegistry.addName(foodTomato, local.foodTomatoName);
+    	LanguageRegistry.addName(foodStrawberry, local.foodStrawberryName);
+    	LanguageRegistry.addName(foodBlueberry, local.foodBlueberryName);
+    	LanguageRegistry.addName(foodGreenTomato, local.foodGreenTomatoName);
+    	LanguageRegistry.addName(foodOrange, local.foodOrangeName);
+    	LanguageRegistry.addName(foodLettuce, local.foodLettuceName);
+    	LanguageRegistry.addName(foodEggplant, local.foodEggplantName);
+    	LanguageRegistry.addName(foodEggScramled, local.foodEggScramledName);
     	LanguageRegistry.addName(foodRawbacon, local.foodRawbaconName);
     	LanguageRegistry.addName(foodBacon, local.foodBaconName);
     }
